8f5384e8912562d4f61dba99eaa62420bfa7b538
Matheus Girardi <matheusmgirardi@gmail.com>
2025-11-10 20:58:35 -0300

    feat: implemented crossover

--- tde-3-genetic-algorithm/src/main.py
+++ tde-3-genetic-algorithm/src/main.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 @dataclass
 class Solution:
     gene:str
-    fitness:float | None
+    fitness: float | None = None
 
 
 
@@ -22,8 +22,21 @@ def tournament_selection(solutions_w_fitness:list[Solution], tournament_size:int
     return mating_pool
 
 def uniform_crossover(parent_a:Solution, parent_b:Solution, mutation_chance:float = 0.05):
-    genes = ['A', 'B']
-    mask = # TODO implementing the mask generation method, should generate a mask and its mirror one.
+    mask = random.choices(['A', 'B'], k=30)
+    inv_mask = ['B' if k =='A' else 'A' for k in mask ]
+    child1 = []
+    child2 = []
+    for i in range(30):
+        if mask[i] == 'A':
+            child1.append(parent_a.gene[i])
+        else:
+            child1.append(parent_b.gene[i])
+        if inv_mask[i] == 'A':
+            child2.append(parent_a.gene[i])
+        else:
+            child2.append(parent_b.gene[i])
+    # returns two new childs
+    return Solution(''.join(child1)), Solution(''.join(child2))
 
     # TODO Remember to add mutation here
 
